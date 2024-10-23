17959c24528a2ec2ce75cc77fa0d0c8b81f71505
hjlee3114 <hwajin3114@gmail.com>
2024-10-23 22:05:07 +0900

    contentlayer,  mdx, 추가 라이브러리 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import mdx from '@next/mdx';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeHighlight from 'rehype-highlight';
 
-export default nextConfig;
+const mdxOptions = {
+  extension: /\.mdx?$/,
+  options: {
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: 'github-light',
+          onVisitLine(node) {
+            if (node.children.length === 0) {
+              node.children.push({ type: 'text', value: ' ' });
+            }
+          },
+        },
+      ],
+      rehypeHighlight,
+    ],
+  },
+};
+
+const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+};
+
+export default mdx(nextConfig, mdxOptions);
