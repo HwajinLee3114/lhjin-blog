6fa83539ae3518b45a07f0e81480342d7889ea8a
hjlee3114 <hwajin3114@gmail.com>
2024-10-26 23:31:51 +0900

    테마별 아이콘 적용

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -9,15 +9,29 @@ const ThemeSwitcher: React.FC = () => {
   return (
     <div className="flex  gap-3">
       {currTheme !== "light" && (
-        <button onClick={() => setTheme("light")}>Light</button>
+        <button onClick={() => setTheme("light")}>
+          <img
+            src="/images/suny-100.png"
+            alt="light mode"
+            className="w-7 h-7"
+          />
+        </button>
       )}
 
       {currTheme !== "dark" && (
-        <button onClick={() => setTheme("dark")}>Dark</button>
+        <button onClick={() => setTheme("dark")}>
+          <img src="/images/moon-100.png" alt="dark mode" className="w-7 h-7" />
+        </button>
       )}
 
       {currTheme !== "custom" && (
-        <button onClick={() => setTheme("custom")}>Custom</button>
+        <button onClick={() => setTheme("custom")}>
+          <img
+            src="/images/snowb-100.png"
+            alt="custom mode"
+            className="w-8 h-8"
+          />
+        </button>
       )}
     </div>
   );
