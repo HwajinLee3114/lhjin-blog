4d317882fe2b5b60281faa31f52d8728921a361d
hjlee3114 <hwajin3114@gmail.com>
2024-10-30 20:24:21 +0900

    blog) 목록 처리 시 useState 제외 후 서버 사이드로 처리 수정

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,6 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import BlogLists from "@/components/comn/blog/BlogLists";
 import Categories from "@/components/comn/blog/Categories";
-import { Post } from "@/types/types";
+import { getSortedPostsData } from "@/utils/blog/getPosts";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -11,30 +8,20 @@ export const metadata: Metadata = {
   description: "이화진 Blog 메인 페이지",
 };
 
-export default function Page() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState<string>("ALL");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("/api/posts"); // API 엔드포인트 호출
-      const data = await response.json();
-      setPosts(data);
-    };
-
-    fetchData();
-  }, []);
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string };
+}) {
+  const { ctg: selectCtg, q: query } = searchParams;
+  const posts = getSortedPostsData();
 
   return (
     <div className="flex gap-5 h-full w-full flex-col md:flex-row">
-      <Categories
-        posts={posts}
-        selectCategory={selectedCategory}
-        setSelectCategory={setSelectedCategory}
-      />
+      <Categories posts={posts} selectCategory={selectCtg} />
       <div className="flex-1">
-        <p className="font-bold text-xl mb-3">모아보기 ({posts.length})</p>
-        <BlogLists posts={posts} selectedCategory={selectedCategory} />
+        {/* <p className="font-bold text-xl mb-3">모아보기</p> */}
+        <BlogLists posts={posts} selectedCategory={selectCtg} />
       </div>
       {/* <aside className="w-40 lg:w-64">
         <div>sdf</div>
--- src/components/comn/blog/Categories.tsx
+++ src/components/comn/blog/Categories.tsx
@@ -6,14 +6,9 @@ import Link from "next/link";
 interface CategoriesProps {
   posts: Post[];
   selectCategory: string;
-  setSelectCategory: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Categories: React.FC<CategoriesProps> = ({
-  posts,
-  selectCategory,
-  setSelectCategory,
-}) => {
+const Categories: React.FC<CategoriesProps> = ({ posts, selectCategory }) => {
   const categories = posts.reduce(
     (acc: { [key: string]: number }, post) => {
       const category = post.category;
@@ -22,21 +17,12 @@ const Categories: React.FC<CategoriesProps> = ({
     },
     { ALL: posts.length }
   );
-
-  const handleCategoryClick = (category: string) => {
-    setSelectCategory(category); // 카테고리 클릭 시 상태 업데이트
-  };
-
   return (
     <aside className="mt-2 lg:mt-0 lg:w-52 xl:w-64">
       <div className="font-bold text-xl px-2 py-3">CATEGORIES</div>
       <ul className="flex flex-col gap-2 p-2">
         {Object.entries(categories).map(([category, count]) => (
-          <Link
-            key={category}
-            href={`/blog?categorie=${category}`}
-            onClick={() => handleCategoryClick(category)}
-          >
+          <Link key={category} href={`/blog?ctg=${category}`}>
             <li
               className={`flex gap-2 px-2 py-3 rounded-md l_ctg-item l_hover-custom ${selectCategory === category ? "selected" : ""}`}
             >
