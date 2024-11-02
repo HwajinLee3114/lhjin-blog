b371a615ffc06ad0dd4a816def2169e38f7e2630
hjlee3114 <hwajin3114@gmail.com>
2024-11-02 21:05:34 +0900

    layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import "@/styles/globals.css";
 import "@/styles/pretty.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import StyleProvider from "@/components/StyleProvider";
 import GlobalStyle from "@/styles/GlobalStyle";
 import Header from "@/components/template/header/Header";
@@ -35,29 +35,26 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     useThemeStore,
     (state) => state.currentThemeOption
   );
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (currTheme !== null) {
-      setIsLoading(false);
-    }
-  }, [currTheme]);
+  // useEffect(() => {
+  //   if (currTheme !== null) {
+  //     setIsLoading(false);
+  //   }
+  // }, [currTheme]);
 
   return (
     <html lang="kr">
       <body className="flex flex-col min-h-screen">
-        {isLoading && (
-          <Overlay isVisible={isLoading}>
-            <Spinner />
-          </Overlay>
+        {currTheme && (
+          <StyleProvider>
+            <GlobalStyle />
+            <CustomStyle />
+            <Header />
+            <div className="h-full flex-1 mt-14 p-5">{children}</div>
+            <Footer />
+          </StyleProvider>
         )}
-        <StyleProvider>
-          <GlobalStyle />
-          <CustomStyle />
-          <Header />
-          <div className="h-full flex-1 mt-14 p-5">{children}</div>
-          <Footer />
-        </StyleProvider>
       </body>
     </html>
   );
